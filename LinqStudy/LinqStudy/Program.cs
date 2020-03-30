2d3a6ee9a211aa52d7a6a33a14ceaf269efc687e
hwhhwh1208 <1968281857@qq.com>
2020-03-30 13:13:26 +0800

    追加了Distinct方法

--- LinqStudy/LinqStudy/Program.cs
+++ LinqStudy/LinqStudy/Program.cs
@@ -6,52 +6,70 @@ using System.Threading.Tasks;
 
 namespace LinqStudy
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-			List<Student> studentList = new List<Student>() { new Student() { StudentID = "1", StudentName = "张三", Age = 18 }, 
-															  new Student() { StudentID = "2", StudentName = "李四", Age = 19 },
-															  new Student() { StudentID = "3", StudentName = "王五", Age = 20 }};
+            List<Student> studentList = new List<Student>() { new Student() { StudentID = "1", StudentName = "张三", Age = 18 },
+                                                              new Student() { StudentID = "2", StudentName = "李四", Age = 19 },
+                                                              new Student() { StudentID = "3", StudentName = "王五", Age = 20 },
+                                                              new Student() { StudentID = "4",StudentName = "张三",Age = 21},
+                                                              new Student() { StudentID = "5",StudentName = "李四",Age = 22},
+                                                              new Student() { StudentID = "6",StudentName = "王五",Age = 23},};
 
-			List<Cursor> cursorList = new List<Cursor>() {	  new Cursor() { StudentID = "1", CursorName = "语文", Grade = 90 }, 
-															  new Cursor() { StudentID = "2", CursorName = "数学", Grade = 88 },
-															  new Cursor() { StudentID = "3", CursorName = "英语", Grade = 70 }};
-			Test3(studentList, cursorList);
-			Console.Read();
-		}
+            List<Cursor> cursorList = new List<Cursor>() {    new Cursor() { StudentID = "1", CursorName = "语文", Grade = 90 },
+                                                              new Cursor() { StudentID = "2", CursorName = "数学", Grade = 88 },
+                                                              new Cursor() { StudentID = "3", CursorName = "英语", Grade = 70 }};
 
-		public static void Test(string[] strs)
-		{
-			var selectStr = from Name in strs
-							where Name.Length < 5
-							select Name;
-			foreach (var item in selectStr)
-			{
-				Console.WriteLine("长度小于5的字符串：{0}", item);
-			}
-		}
+            TestDistinct(studentList);
+            Test3(studentList, cursorList);
+            Console.Read();
+        }
 
-		public static void Test2(string[] strs)
-		{
-			var selectStr = from Str in strs
-							where Str.Length < 5
+        public static void Test(string[] strs)
+        {
+            var selectStr = from Name in strs
+                            where Name.Length < 5
+                            select Name;
+            foreach (var item in selectStr)
+            {
+                Console.WriteLine("长度小于5的字符串：{0}", item);
+            }
+        }
 
-							select Str;
-		}
+        public static void Test2(string[] strs)
+        {
+            var selectStr = from Str in strs
+                            where Str.Length < 5
 
-		public static void Test3(List<Student> list1, List<Cursor> list2)
-		{
-			var coll = from p in list1
-					   from c in list2
-					   where p.StudentID == c.StudentID && c.Grade > 80
-					   select new { p, c };
+                            select Str;
+        }
 
-			foreach (var item in coll)
-			{
-				Console.WriteLine("姓名：{0}，科目：{1}，成绩：{2}", item.p.StudentName, item.c.CursorName, item.c.Grade);
-			}
-		}
-	}
+        public static void Test3(List<Student> list1, List<Cursor> list2)
+        {
+            var coll = from p in list1
+                       from c in list2
+                       where p.StudentID == c.StudentID && c.Grade > 80
+                       select new { p, c };
+
+            foreach (var item in coll)
+            {
+                Console.WriteLine("姓名：{0}，科目：{1}，成绩：{2}", item.p.StudentName, item.c.CursorName, item.c.Grade);
+            }
+        }
+
+        public static void TestDistinct(List<Student> stuList)
+        {
+            var stuData = from stus in stuList.Distinct(new StudentCompare())
+                          select stus;
+
+            foreach (var item in stuData)
+            {
+                Console.WriteLine("学生ID：" + item.StudentID + "---学生姓名：" + item.StudentName);
+            }
+        }
+
+
+    }
 }
--- LinqStudy/LinqStudy/Student.cs
+++ LinqStudy/LinqStudy/Student.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace LinqStudy
 {
-	public class Student
-	{
-		public string StudentID { get; set; }
+    public class Student
+    {
+        public string StudentID { get; set; }
 
-		public string StudentName { get; set; }
+        public string StudentName { get; set; }
 
-		public int Age { get; set; }
-	}
+        public int Age { get; set; }
+    }
+
+    public class StudentCompare : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x == null || y == null)
+            {
+                return false;
+            }
+            else
+            {
+                return x.StudentName == y.StudentName;
+            }
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return obj.StudentName.GetHashCode();
+            }
+        }
+    }
 }
